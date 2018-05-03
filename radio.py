2d1ea92cf482f1f306c8d694f986593b6bdf5226
stevencrack <32911023+stevencrack@users.noreply.github.com>
2018-05-03 12:22:52 -0500

    Add files via upload

---
+++ radio.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+Prob0a1 = 0
+Prob1a20 = 1
+
+
+def lambda(l):
+    if((l < 1) or (l > 100):
+
+       return 0
+
+    else:
+       return 1/99
+
+def distribucion(l, x):
+
+	distr = (1/l)*np.exp((-1)*(x/l))
+	return distribucion
+
+x = [1.2, 2.5, 2.8, 5.0]
+
+#def prob_x
